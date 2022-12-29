1ef9474bbdf0bd97be2854b44952cf48077f25a0
Julie J <jeannet.julie@gmail.com>
2022-12-29 17:50:24 +0100

    Same cross components communication using Subject

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,15 +1,21 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  // custom event creation
-  nameEmitter = new EventEmitter<string>
+  // with custom event creation
+  // nameEmitter = new EventEmitter<string>
+  // achieving the same with subject
+  nameEmitter=new Subject<string>()
 
   // methode to raise this event
   raiseNameEmitterEvent(data: string) {
-    this.nameEmitter.emit(data)
+    // this.nameEmitter.then(data)
+    // with Subject, instead of emit method, we will use next
+    this.nameEmitter.next(data)
   }
 
 
