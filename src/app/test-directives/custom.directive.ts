edaff6797e5cb2b39d37f4ea076dddac0d7c3c2f
Julie J <jeannet.julie@gmail.com>
2023-01-03 08:21:10 +0100

    @HostListener in a Custom Directive...

--- src/app/test-directives/custom.directive.ts
+++ src/app/test-directives/custom.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCustom]'
@@ -9,9 +9,18 @@ export class CustomDirective implements OnInit {
 
   ngOnInit(): void {
 
-    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', '#f1948A')
-    this.renderer.setStyle(this.element.nativeElement, 'padding', '2rem')
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', '#74992e')
+    this.renderer.setStyle(this.element.nativeElement, 'padding', '1rem')
+    this.renderer.setStyle(this.element.nativeElement, 'transition', '0.5s')
+  }
 
+  // ajout d'un hostListener pour implémenter une méthode (event handler) liée à un évènement js
+  // ici, c'est une aberration puisqu'on pourrait faire du style avec :hover mais je présume qu'on peut imaginer mettre un maximum d'instructions js dans un même HostListener
+  @HostListener('mouseenter') hostMouseOver(){
+    this.renderer.setStyle(this.element.nativeElement, "padding", '2rem')
+  }
+  @HostListener('mouseout') hostMouseOut(){
+    this.renderer.setStyle(this.element.nativeElement, "padding", '1rem')
   }
 
 }
