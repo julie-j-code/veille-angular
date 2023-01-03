73fe5e61a9b96b97510ad975388897e84a0a6716
Julie J <jeannet.julie@gmail.com>
2023-01-03 08:58:06 +0100

    @HostBinding testing... waf !!!

--- src/app/test-directives/custom.directive.ts
+++ src/app/test-directives/custom.directive.ts
@@ -1,9 +1,11 @@
-import { Directive, ElementRef, OnInit, Renderer2, HostListener } from '@angular/core';
+import { parseHostBindings } from '@angular/compiler';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appCustom]'
 })
 export class CustomDirective implements OnInit {
+
 
   constructor(private element:ElementRef, private renderer:Renderer2) { }
 
@@ -16,11 +18,17 @@ export class CustomDirective implements OnInit {
 
   // ajout d'un hostListener pour implémenter une méthode (event handler) liée à un évènement js
   // ici, c'est une aberration puisqu'on pourrait faire du style avec :hover mais je présume qu'on peut imaginer mettre un maximum d'instructions js dans un même HostListener
+
+  @HostBinding('style.padding') padding:string ='#1rem';
+
+  // c'est ce qu'on a nommé padding pour se référer à style.padding qui devient la référence pour toutes les instsructions du HostListener... 
   @HostListener('mouseenter') hostMouseOver(){
-    this.renderer.setStyle(this.element.nativeElement, "padding", '2rem')
+    this.padding="2rem"
+    // this.renderer.setStyle(this.element.nativeElement, "padding", '2rem')
   }
   @HostListener('mouseout') hostMouseOut(){
-    this.renderer.setStyle(this.element.nativeElement, "padding", '1rem')
+    this.padding="1rem"
+    // this.renderer.setStyle(this.element.nativeElement, "padding", '1rem')
   }
 
 }
