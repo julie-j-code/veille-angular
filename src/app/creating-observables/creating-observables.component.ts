e8bd1234faebfd30d1cd90e2c59988dfaf27f178
Julie J <jeannet.julie@gmail.com>
2023-01-05 15:48:26 +0100

    Different ways of Creating Observable | 2 Using Create Method from Observable

--- src/app/creating-observables/creating-observables.component.ts
+++ src/app/creating-observables/creating-observables.component.ts
@@ -8,14 +8,28 @@ import { Observable } from 'rxjs';
 export class CreatingObservablesComponent implements OnInit {
 
   // method using Observable constructor
-  myObservable = new Observable((observer) => {
+  // myObservable = new Observable((observer) => {
+  //   console.log('Obserable starts ! ')
+  //   setTimeout(() => { observer.next("1") }, 1000)
+  //   setTimeout(() => { observer.next("2") }, 2000)
+  //   setTimeout(() => { observer.next("3") }, 3000)
+  //   // setTimeout(()=>{observer.error(new Error("Something went wrong ! Please, try again later"))}, 3000)
+  //   setTimeout(() => { observer.next("4") }, 4000)
+  //   setTimeout(() => { observer.next("5") }, 5000)
+  //   // setTimeout(()=>{observer.complete()},3000)
+  //   // observer.next("1")
+  //   // observer.next("2")
+  //   // observer.next("3")
+  // })
+
+  // Method using the Create Method from Observable Class
+  myObservable = Observable.create((observer:any) => {
     console.log('Obserable starts ! ')
-    setTimeout(() => { observer.next("1") }, 1000)
-    setTimeout(() => { observer.next("2") }, 2000)
-    setTimeout(() => { observer.next("3") }, 3000)
-    // setTimeout(()=>{observer.error(new Error("Something went wrong ! Please, try again later"))}, 3000)
-    setTimeout(() => { observer.next("4") }, 4000)
-    setTimeout(() => { observer.next("5") }, 5000)
+    setTimeout(() => { observer.next("A") }, 1000)
+    setTimeout(() => { observer.next("B") }, 2000)
+    setTimeout(() => { observer.next("C") }, 3000)
+    setTimeout(() => { observer.next("D") }, 4000)
+    setTimeout(() => { observer.next("E") }, 5000)
     // setTimeout(()=>{observer.complete()},3000)
     // observer.next("1")
     // observer.next("2")
@@ -23,9 +37,9 @@ export class CreatingObservablesComponent implements OnInit {
   })
 
   ngOnInit(): void {
-    this.myObservable.subscribe((val) => {
+    this.myObservable.subscribe((val:any) => {
       console.log(val)
-    }, (error) => {
+    }, (error:any) => {
       alert(error.message)
     }, () => {
       alert('Observable has complet emitting all values')
