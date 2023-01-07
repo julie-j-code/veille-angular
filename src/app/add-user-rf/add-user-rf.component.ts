bedc83c36c8a9442d0774e5c8b4b007ca990b5f3
Julie J <jeannet.julie@gmail.com>
2023-01-07 17:08:09 +0100

    Reactive Form Demo | Grouping of Form Controls : WAF ! I do not like it !!!!

--- src/app/add-user-rf/add-user-rf.component.ts
+++ src/app/add-user-rf/add-user-rf.component.ts
@@ -9,7 +9,7 @@ import { UsersService } from '../services/users.service';
   styleUrls: ['./add-user-rf.component.css']
 })
 export class AddUserRfComponent implements OnInit {
-  title:string="Reactive Forms Demo"
+  title: string = "Reactive Forms Demo"
   // https://stackoverflow.com/questions/66603888/where-to-initialise-a-formgroup-in-reactive-forms
   myReactiveForm: FormGroup = new FormGroup({});
 
@@ -20,19 +20,20 @@ export class AddUserRfComponent implements OnInit {
   ngOnInit(): void {
     this.myReactiveForm = new FormGroup({
       // https://angular.io/api/forms/Validators
-      name: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      gender: new FormControl('other'),
+      personalDetails: new FormGroup({
+        name: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        gender: new FormControl('other')
+      }),
       status: new FormControl('active')
     })
 
   }
 
-
   addUser() {
     // we do not need key reference, cause we already have a ref : myReactiveForm
     console.log(this.myReactiveForm);
-    this.userService.AddNewUser(this.myReactiveForm.value.name,this.myReactiveForm.value.status)
+    this.userService.AddNewUser(this.myReactiveForm.value.personalDetails.name, this.myReactiveForm.value.status)
 
   }
 
